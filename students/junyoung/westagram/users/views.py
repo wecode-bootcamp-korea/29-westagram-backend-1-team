d9feeb97586656c00e68e60f3a95ea2312679c2f
aerialslash <33915211+aerialslash0422@users.noreply.github.com>
2022-01-14 15:08:00 +0900

    [김준영] Mission 3 : 회원가입 수정사항 반영

--- students/junyoung/westagram/users/views.py
+++ students/junyoung/westagram/users/views.py
@@ -8,34 +8,42 @@ from users.models import User
 
 class SignUpView(View):
     def post(self,request):
-        data                    = json.loads(request.body)
-        email_validation        = '\w+[@]+\w+[.]+\w+'
-        password_validation     = '^(?=.*\D)(?=.*\d)(?=.*\W).{8,}'
-        phone_number_validation = '^[010]{1}\d{8}'
-                
-        if not data["email"] or not data["password"]:
-            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+        try:
+            data                    = json.loads(request.body)
+            email                   = data['email']
+            password                = data['password']
+            phone_number            = data['phone_number']
+            email_validation        = r'\w+[@]+\w+[.]+\w+'
+            password_validation     = r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W).{8,}'
+            phone_number_validation = r'^(010)\d{8}$'
+            
+            if not email or not password:
+                raise KeyError
+            
+            elif User.objects.filter(email = email).exists():
+                return JsonResponse({"message" : "DUPLICATED_EMAIL"}, status = 409)
+            
+            elif not re.match(email_validation, email):
+                return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
+                    
+            elif not re.match(password_validation, password):
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
         
-        elif not re.match(email_validation, data["email"]):
-            return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
-        
-        elif User.objects.filter(email = data["email"]).exists():
-            return JsonResponse({"message" : "DUPLICATED_EMAIL"}, status = 409)
-        
-        elif not re.match(password_validation, data["password"]):
-            return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
-        
-        elif not re.match(phone_number_validation, data["phone_number"]):
-            return JsonResponse({"message" : "INVALID_PHONE_NUMBER"}, status = 400)
-                
-        else:
+            elif not re.match(phone_number_validation, phone_number):
+                return JsonResponse({"message" : "INVALID_PHONE_NUMBER"}, status = 400)
+            
             User.objects.create(
                 name         = data["name"],
-                email        = data["email"],
-                password     = data["password"],
-                phone_number = data["phone_number"],
-            )   
-            return JsonResponse({"message" : "SUCCESS"}, status = 201)
+                email        = email,
+                password     = password,
+                phone_number = phone_number
+            )
+            return JsonResponse({"message" : "SUCCESS"}, status = 201)   
+        
+        except KeyError as e:
+            return JsonResponse({"message" : f"KEY_ERROR : ENTER_YOUR_{e.args[0].upper()}"}, status = 400)
+        
+
         
         
         
